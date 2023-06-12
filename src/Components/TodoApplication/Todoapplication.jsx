d047e975040270102c82501d4e43c62446f788ba
TAMS-TECHNOLOGY <dummytwiz@gmail.com>
2023-06-12 13:04:42 +0100

    default files and folder for my todo app

---
+++ src/Components/TodoApplication/Todoapplication.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Todoapplication.css";
+
+function Todoapplication() {
+  return <div className="todo_container">Todoapplication</div>;
+}
+
+export default Todoapplication;
